59bdbdc269cbd82ffa3c8efc23a578a89f2d1844
olena <vasechko.elenaa@gmail.com>
2022-08-18 23:02:19 +0300

    change menu by jquery

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,9 @@ $(function () {
     $('.dropdown').hover(function () {
         $('.submenu').toggleClass('dropdown-show');
     });
+
+    $('.menu-btn').click(function () {
+        $('.menu-btn ~ nav, .menu-btn').toggleClass('open-burger-menu')
+        $('body').toggleClass('lock')
+    });
 });
