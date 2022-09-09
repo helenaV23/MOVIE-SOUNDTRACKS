d7905be60066ca6562e6ea75b183ad5ea2405797
olena <vasechko.elenaa@gmail.com>
2022-09-09 22:12:42 +0300

    add modal

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,32 @@
 $(function () {
-    $('.dropdown').hover(function () {
-        $('.submenu').toggleClass('dropdown-show');
-    });
-
-    $('.menu-btn').click(function () {
+    $('.menu-btn').on('click', function () {
         $('.menu-wrapper').toggleClass('open-burger-menu');
         $('.menu-btn').toggleClass('menu-btn-active');
         $('body').toggleClass('lock');
     });
 
-    $('.submenu-link').click(function () {
+    $('.submenu-link').on('click', function () {
         $('body').removeClass('lock');
         $('.menu-wrapper').removeClass('open-burger-menu');
         $('.menu-btn').removeClass('menu-btn-active');
     });
+
+    $('.js-listen').on('click', function () {
+        $('.modal-overlay').addClass('modal-open');
+        $('body').addClass('lock');
+    });
+
+    closeModal('.modal-overlay');
+    closeModal('.modal-close-btn');
+
+    $('.modal-container').on('click', function (e) {
+        e.stopPropagation();
+    });
+
+    function closeModal(selector) {
+        $(selector).on('click', function () {
+            $('.modal-overlay').removeClass('modal-open');
+            $('body').removeClass('lock');
+        });
+    }
 });
